59cf45e74b609dc8139ad7f97e8636a65be5e8b6
Fanny Baquey <fanny.baquey@gmail.com>
2023-10-12 15:39:51 +0200

    feat: séparation des constantes, ajout de la fonction compare

---
+++ constantes.js
@@ -0,0 +1,32 @@
+const triggers = [
+    ["hi", "hey", "hello", "good day"],
+    ["how are you", "how are things"],
+    ["what is the weather", "what's the weather"],
+    ["what is going on", "what's going on", "what's up"],
+    ["happy", "good", "great", "fine", "blessed"],
+    ["sad", "bad", "terrible"],
+    ["tell me a story", "tell me a joke"],
+    ["thanks", "thank you"],
+    ["bye", "goodbye", "good bye", "see you later"],
+];
+
+const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    ["Fine... how are you?", "Pretty well, how are you?", "Fantastic, how are you?"],
+    ["It's a beautiful day today", "It's a rainy day", "It's a sunny day"],
+    ["Nothing much", "Exciting things"],
+    ["Glad to hear it", "That's great to hear"],
+    ["Why?", "Cheer up!"],
+    ["What about ?", "Once upon a time..."]
+    ["You're welcome", "No problem"],
+    ["Bye", "See you later"],
+]
+
+const alternatives = [
+    "Same",
+    "Try again",
+    "I don't understand",
+    "Sorry, I didn't get that",
+    "Try again",
+    "Bro..."
+]
--- index.js
+++ index.js
@@ -1,9 +1,62 @@
+import { triggers, replies, alternatives } from "./constantes.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+
     const inputField = document.querySelector('#input');
-    inputField.addEventListener('keydown', function (e) {
-        if (e.code === "Enter") {
-            console.log("Vous avez cliqué sur le formulaire et pressé Entrer")
+
+    inputField.addEventListener('keydown', (event) => {
+
+        // Si on appuie sur la touche entrée
+        if (event.code === "Enter") {
+
+            // Récupère la valeur de l'input
+            let input = inputField.value;
+            document.getElementById("user").innerHTML = input;
+            output(input);
         };
     });
+
+    function output(input) {
+        let product;
+        let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+        document.getElementById('chatbot').innerHTML = product;
+        speak(product);
+
+        document.getElementById("input").value = "";
+        text = text
+            .replace(/ a /g, " ")
+            .replace(/i feel /g, "")
+            .replace(/whats/g, "what is")
+            .replace(/please /g, "")
+            .replace(/ please/g, "")
+
+        if (compare(trigger, reply, text)) {
+            // recherche pour une correspondance dans trigger
+            product = compare(trigger, reply, text);
+
+        } else if (text.match(/robot/gi)) {
+            product = robot[Math.floor(Math.random() * robot.length)];
+        } else {
+            product = alternative[Math.floor(Math.random() * alternative.length)];
+        }
+
+        addChat(input, product);
+    }
+    
+    const robot = ["How do you do Human ?"]
+
+   
+
+    function compare(triggerArray, replyArray, text) {
+        let item;
+        for (let x = 0; x < triggerArray.length; x++) {
+            for (let y = 0; y < replyArray.length; y++) {
+                if(triggerArray[x][y] === text) {
+                    items = replyArray[x];
+                    item = items[Math.floor(Math.random()* items.length)];
+                }
+            }
+        }
+        return item;
+    }
 });
